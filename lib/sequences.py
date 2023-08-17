af6bb99d1308f929f80cfc6e2545bbd40d31a75d
judy-chelangat <judysigilai75@gmail.com>
2023-08-17 18:02:59 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fib_numbers=[0,1]
+    for i in range(2,length):
+       fib_numbers.append(fib_numbers[i - 1] + fib_numbers[i - 2])
+       print(fib_numbers)
+
     pass
